20e4da3ca44cc0c59f4da1a1e85fc502cb71368f
万明珠 <mingzhuwan@gmail.com>
2021-09-14 23:28:50 +1000

    updated App.js <div> tag to <> tag

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
-// import DailyWeather from "./components/DailyWeather";
 import LandingPage from "./components/LandingPage/LandingPage";
-// import TemperatureWithInsolation from "./components/TemperatureWithInsolation";
-// import WeatherWithDescription from "./components/WeatherWithDescription";
-// import SearchBar from "./components/SearchBar";
-// import { library } from '@fortawesome/fontawesome-svg-core';
-// import { fab } from '@fortawesome/free-brands-svg-icons';
-// <FontAwesomeIcon icon="coffee" />
-
-// get our fontawesome imports
-// import { faHome } from "@fortawesome/free-solid-svg-icons";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
 
 function App() {
   return (
-    <div>
+    <>
       <LandingPage />
-    </div>
+    </>
   );
 }
 
