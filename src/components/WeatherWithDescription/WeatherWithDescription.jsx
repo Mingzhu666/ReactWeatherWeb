7fa63e04d7b705900d8d104e9d9aee64c4fb001d
万明珠 <mingzhuwan@gmail.com>
2021-09-14 23:40:34 +1000

    updated WeatherWithDescription.jsx to function component

--- src/components/WeatherWithDescription/WeatherWithDescription.jsx
+++ src/components/WeatherWithDescription/WeatherWithDescription.jsx
@@ -1,21 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './WeatherWithDescription.scss';
 import WeatherTypeIcon from '../WeatherTypeIcon/WeatherTypeIcon';
 import WeatherTypeDescription from '../WeatherTypeDescription/WeatherTypeDescription';
 
-class WeatherWithDescription extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div className="weather-with-description">
-        <WeatherTypeIcon className="weather-with-description__icon" weatherTypeImage={this.props.weatherTypeImage} />
-        <WeatherTypeDescription className="weather-with-description__description" weatherTypeText={this.props.weatherTypeText} />
-      </div>
-    )
-  }
+const WeatherWithDescription = ({ weatherTypeImage, weatherTypeText }) => {
+  return (
+    <div className="weather-with-description">
+      <WeatherTypeIcon className="weather-with-description__icon" weatherTypeImage={weatherTypeImage} />
+      <WeatherTypeDescription className="weather-with-description__description" weatherTypeText={weatherTypeText} />
+    </div>
+  );
 }
 
 export default WeatherWithDescription;
